6dce36c81ad3a1a23a7e41602029d82a8fbc300f
charlie <charlieforce1999@gmail.com>
2017-04-28 01:44:39 +0000

    We did it reddit

--- highest_number_cubed/main.py
+++ highest_number_cubed/main.py
@@ -2,4 +2,36 @@
 
 
 def highest_number_cubed(limit):
-    pass
+  number  = 0
+   
+  while True:
+      number += 1
+      if number ** 3 > limit:
+          return number - 1
+    
+    
+# def highest_number_cubed(limit):
+#     previous_number  = 1
+        
+#     while True:
+#         current_number = previous_number + 1
+#         if current_number ** 3 > limit:
+#             return previous_number
+            
+#     previous_number = current_number
+            
+
+# def highest_number_cubed(limit):
+#   number  = 0
+   
+#   while True:
+#       number += 1
+#       if number ** 3 > limit:
+#           return number - 1
+
+# def highest_number_cubed(limit):
+    
+#     floater = limit**(1./3)
+#     return int(floater)
+    
+    
